7926bc92874e7b3c153d069d552d98077e13eb4c
fthebaud <fthebaud@gmail.com>
2024-12-12 16:05:05 +0100

    Tuto until bonus step (collection)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 // @ts-check
-// @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // accessible via context.site ?
+  site: "https://example.com",
+
+  integrations: [preact()],
+});
